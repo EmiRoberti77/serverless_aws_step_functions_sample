0b3a8b62bb0e3a0b66b2c1098f610615120c3be2
EmiRoberti77 <emi@datamotion.ai>
2024-08-11 19:33:34 +0100

    added choice statemet in the .ymal

--- handler.js
+++ handler.js
@@ -1,7 +1,25 @@
+const MAX = 60;
+
+class NumberIsTooBig extends Error {
+  constructor(n) {
+    super(`${n} is too big`);
+    this.name = "NumberIsTooBig";
+    Error.captureStackTrace(this, NumberIsTooBig);
+  }
+}
+
 exports.add = async ({ x, y }) => {
   return x + y;
 };
 
 exports.double = async (n) => {
+  const total = n * 2;
+  if (total > MAX) {
+    throw new NumberIsTooBig(total);
+  }
+  return total;
+};
+
+exports.doubleBigNumbers = async (n) => {
   return n * 2;
 };
